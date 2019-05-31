57cbee362c9d9df0596a28b8c1d5da6ec35475a2
AGEBW <a.d.river@hotmail.com>
2019-05-31 08:28:27 -0600

    Proyecto de lista de tareas concluido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import {Tarea} from './models/tarea';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ListaTareas';
+  tareaArray: Tarea[] = [
+    {id: 1, tarea: 'proyecto1', materia: 'empresariales'},
+    {id: 2, tarea: 'proyecto2', materia: 'empresariales'},
+    {id: 3, tarea: 'tareas', materia: 'empresariales'}
+  ];
+
+  selectedTarea: Tarea = new Tarea();
+  openForEdit( tarea: Tarea) {
+    this.selectedTarea = tarea;
+  }
+  addOrEdit() {
+    if (this.selectedTarea.id === 0) {
+      //este campo es para insertar
+      this.selectedTarea.id = this.tareaArray.length + 1;
+      this.tareaArray.push(this.selectedTarea);
+    }
+    //esto es para actualizar
+    this.selectedTarea = new Tarea();
+  }
+  delete(){
+    if (confirm('La tarea ah sido concluida?')) {
+      this.tareaArray = this.tareaArray.filter(x => x != this.selectedTarea);
+    this.selectedTarea = new Tarea();
+    } }
 }
---
+++ src/app/models/tarea.ts
@@ -0,0 +1,5 @@
+export class Tarea {
+    id: number;
+    tarea: string;
+    materia: string;
+}
